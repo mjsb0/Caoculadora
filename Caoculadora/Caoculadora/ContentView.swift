2be097e52e67e832eebe95f45e65d323dd8d1c4b
Maria Julia Souza Balbino <julia.sbalbino@outlook.com>
2024-05-08 18:03:53 -0300

    Adiciona imagem de clarinha e implementa inicialmente o resultado

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State var years: Int? = nil
     @State var months: Int? = nil
+    @State var result: Int?
         
     var body: some View {
         VStack(alignment: .leading) {
@@ -21,15 +22,48 @@ struct ContentView: View {
             Text("Meses")
             TextField("Digite quantos meses além disso ele tem", value: $months, format: .number)
             
+            Text("Porte")
+            // aqui vai ficar o segmentes control
+            
+            if let result {
+                Text("Seu cachorro tem em idade humana...")
+                Text("\(result) anos")
+                    .foregroundStyle(.purple)
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 20)
+            }
+            
+            Button(action: {
+                result = 23
+            }, label: {
+                ZStack {
+                    Color.purple
+                    Text("Cãocular")
+                        .foregroundStyle(.white)
+                }
                 
+            })
+            
+            .cornerRadius(12)
+            .frame(height: 50)
+            
         }
+        
+        
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .bold()
         .fontDesign(.rounded)
         .padding()
+        
     }
-}
+    
+    }
 
 #Preview {
     ContentView()
