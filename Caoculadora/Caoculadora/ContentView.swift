6b89e144b9497dd30c3dc6dddc1ad6411ec4f428
Maria Julia Souza Balbino <julia.sbalbino@outlook.com>
2024-05-14 15:21:18 -0300

    Implementa cores, navigationbar, animações, alertas e ajusta layout

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -13,79 +13,127 @@ struct ContentView: View {
     @State var months: Int? = nil
     @State var result: Int?
     @State var porteSelecionado: Porte = .pequeno
+    @State var failedImput: Bool = false
+    @State var zeroImput: Bool = false
+    let noImputAlertTitle: String = "Preencha os campos acima."
+    let zeroImputAlertTitle: String = "Pelo menos um dos campos deve ser preenchido!"
+    
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            Text("Qual a idade do seu cão?")
-                .font(.header5)
-            Text("Anos")
-                .font(.body1)
-            TextField("Digite quantos anos seu cão tem", value: $years, format: .number
-            )
-            Text("Meses")
-                .font(.body1)
-            TextField("Digite quantos meses além disso ele tem", value: $months, format: .number)
-            
-            
-            Text("Porte")
-                .font(.body1)
-            
-            Picker("Porte", selection: $porteSelecionado) {
-                ForEach(Porte.allCases, id: \.self) { porte in
-                    Text(porte.rawValue)
-                        .tag(porte)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Qual a idade do seu cão?")
+                        .font(.header5)
+                        .foregroundStyle(.colorCaoculadora)
+                        .padding(.top, 24)
                     
-                }
-    
-            }
-            .pickerStyle(.segmented)
-            
-            Divider()
-                .background(.purple)
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("Anos")
+                            .font(.body1)
+                            .foregroundStyle(.colorCaoculadora)
+                        
+                        TextField("Digite quantos anos seu cão tem", value: $years, format: .number
+                        )
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("Meses")
+                            .foregroundStyle(.colorCaoculadora)
+                            .font(.body1)
+                        TextField("Digite quantos meses além disso ele tem", value: $months, format: .number)
+                    }
+                    
+                    
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("Porte")
+                            .foregroundStyle(.colorCaoculadora)
+                            .font(.body1)
+                        
+                        Picker("Porte", selection: $porteSelecionado) {
+                            ForEach(Porte.allCases, id: \.self) { porte in
+                                Text(porte.rawValue)
+                                    .tag(porte)
+                            }
                             
-            Spacer()
-            
-            if let result {
-                Text("Seu cachorro tem em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.display)
-                    .foregroundStyle(.purple)
+                            
+                            
+                        }
+                        
+                    }
                     
+                    .pickerStyle(.segmented)
                     
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .shadow(radius: 20)
-            }
-            
-            Spacer()
-            
-            Button(action: processyears, label: {
-                ZStack {
-                    Color.purple
-                    Text("Cãocular")
-                        .foregroundStyle(.white)
-                        .font(.body1)
+                    Divider()
+                        .background(.colorCaoculadora)
+                    
+                    Spacer()
+                    
+                    if let result {
+                        Text("Seu cachorro tem em idade humana...")
+                            .font(.body1)
+                            .foregroundStyle(.colorCaoculadora)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        Text("\(result) anos")
+                            .contentTransition(.numericText())
+                            .font(.display)
+                            .foregroundStyle(.colorCaoculadora)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        
+                        
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: 20)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: processyears, label: {
+                        ZStack {
+                            Color.colorCaoculadora
+                            Text("Cãocular")
+                                .foregroundStyle(.white)
+                                .font(.body1)
+                        }
+                        
+                    })
+                    
+                    .cornerRadius(12)
+                    .frame(height: 50)
+                    
                 }
                 
-            })
-            
-            .cornerRadius(12)
-            .frame(height: 50)
+                .containerRelativeFrame(.vertical)
+                
+                .navigationTitle("Cãoculadora")
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarBackground(.colorCaoculadora, for: .navigationBar)
+                .toolbarColorScheme(.dark, for: .navigationBar)
+                
+                
+                
+                
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .bold()
+                .fontDesign(.rounded)
+                .padding()
+                
+                .alert(noImputAlertTitle, isPresented: $failedImput){
+                    Button("Ok", role: .cancel, action: {})
+                }
+                .alert(zeroImputAlertTitle, isPresented: $zeroImput){
+                    Button("Ok", role: .cancel, action: {})
+                }
+                .animation(.easeOut, value: result)
+            }
+            .scrollDismissesKeyboard(.immediately)
             
         }
-        
-        
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .bold()
-        .fontDesign(.rounded)
-        .padding()
         
     }
     
@@ -94,24 +142,29 @@ struct ContentView: View {
             let years,
             let months
         else {
-            print("preencha o campo de entrada")
+            failedImput = true
+            print("Preencha o campo de entrada!")
             return
         }
         guard years > 0 || months > 0 else{
-            print("algum campo tem que ter valor maior que zero")
+            zeroImput = true
+            print("Algum campo tem que ter valor maior que zero.")
             return
         }
         
-        
-        
-        result = porteSelecionado.conversaoDeIdade(
-            anos: years,
-            meses: months)
-        
+        withAnimation(.easeInOut.speed(0.5)) {
+            result = porteSelecionado.conversaoDeIdade(
+                anos: years,
+                meses: months)
         }
         
+        
+        
     }
+    
+}
 
 #Preview {
     ContentView()
 }
+
